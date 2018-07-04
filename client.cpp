c9ffefffae47cb0e81e3ff5b9e9d1fe223d788cc
Navin Ghimire <ghimire.navin@gmail.com>
2018-07-04 12:55:41 -0500

    Added client server account functionality

--- client.cpp
+++ client.cpp
@@ -13,7 +13,8 @@
 #define REQ_DEP 2
 #define REQ_WIT 3
 #define REQ_CLO 4
-
+#define LOGIN_PASS 1;
+#define LOGIN_FAIL 0;
 int main() {	
 	int PORT = 8020;
 	int requestType, account;
@@ -32,21 +33,38 @@ int main() {
 	serveraddress.sin_port = htons(PORT);	
 	inet_pton(AF_INET,"127.0.0.1",&serveraddress.sin_addr);
 	connect(clientsocket,(struct sockaddr *)&serveraddress,sizeof(serveraddress));
-	int loggedIn = false;
-	
+	int loggedIn = -1 ;
 		
 	string request;
 	// create login request:
 	requestType = REQ_LOG;
-
 	request = to_string(requestType) + " " + to_string(account) + " " +  to_string(amount);
 	cout << request << endl;
 	send(clientsocket,request.c_str() ,request.length(),0);
 	int valread = read(clientsocket,buffer,1024);
-
 	// if login successfull 
-	if (valread = 1) {
-			while (true) {
+	if (atoi(buffer) == 1) {
+			while (loggedIn) {
+					cout << "Available Transactions\n"
+							<< "\t 1. Balance\n" 
+							<< "\t 2. Deposit\n"
+							<< "\t 3. Withdraw\n"
+							<< "\t 4. Logont\n";
+					cout << "Transaction: ";
+					cin >> requestType;
+
+					if (requestType == 0 || requestType == 4) {
+						cout << "Account number: ";
+						cin >> account;
+					}
+					if (requestType == 0 || requestType == 1 || requestType == 4) {
+						amount = 0.0;
+					} else {
+						cout << "Amount: ";
+						cin >> amount;
+					}
+					request = to_string(requestType) + " " + to_string(account) + " " +  to_string(amount);
+					send(clientsocket,request.c_str() ,request.length(),0);
 			}
 	}
 	printf("%s",buffer);
--- server.cpp
+++ server.cpp
@@ -2,6 +2,8 @@
 #include <unistd.h>
 #include <netinet/in.h>
 #include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
 #include <iostream>
 #include <fstream>
 #include <sstream>
@@ -13,6 +15,11 @@
 #define REQ_DEP 2
 #define REQ_WIT 3
 #define REQ_CLO 4
+#define LOGIN_PASS 1
+#define LOGIN_FAIL 0
+
+
+
 // struct to hold the individual account information
 struct account {
 	int accountid;
@@ -66,14 +73,43 @@ void handleConnection(int socket) {
 	int requestType;
 	int account;
 	double amount;
-
+	int loggedIn = -1;
 	char buffer[1024];
 	bzero(buffer,1024);
 	n = read(socket,buffer,1024);
-	printf("Message: %s",buffer);
+	printf("Message: %s\n",buffer);
 	stringstream ss;
 	ss << buffer;
 	ss >> requestType >> account >> amount;
-	cout << endl << requestType << endl << account << endl << amount << endl;
+
+	if (loggedIn != -1)  { // if logged in 
+					cout << endl << accounts[loggedIn].username << " logged in" << endl;
+					send(socket,"Already In",10,1); 
+	} else {
+			if (requestType == REQ_LOG) {
+				// if the request is login valid and send info back to the client
+				for (int i = 0; i < 3; i++) {
+					if (accounts[i].accountid == account) {
+						loggedIn = i;
+						break;
+					}
+				}
+				if (loggedIn == -1){
+					send(socket,"0", 2, 0); 
+				} else {
+					send(socket,"1",2,1); 
+				}
+			}
+			switch (requestType) {
+				case REQ_BAL:
+						break;
+				case REQ_DEP:
+						break;
+				case REQ_CLO:
+						break;
+			}
+
+	}
+
 
 }
